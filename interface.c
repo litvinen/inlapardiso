7c1d254827034f70a7db4de2d9cc70b7965f412a
Alexander Litvinenko <litvinen@tu-bs.de>
2018-01-14 16:36:33 +0300

    First files

---
+++ interface.c
@@ -0,0 +1,56 @@
+// Here Alex will implement his part
+
+// reordering, computed just once
+int alex_reordering(data_storage* mydata_storage, pgraph* mgraph)
+{
+  return 0;
+}
+
+//compute the symbolic factorization, computed just once
+int alex_symbolic_factorisation(data_storage* mydata_storage)
+{
+  return 0;
+}
+
+//Cholesky factorization
+int alex_chol(data_storage* mydata_storage, pmatrix Q)
+{
+  return 0;
+}
+
+//Solve linear syatem Lx=b
+int alex_solve_Lx(data_storage* mydata_storage,  double* b)
+{
+  return 0;
+}
+
+
+//Solve linear system L^Tx=b
+int alex_solve_LTx(data_storage* mydata_storage,  double* b)
+{
+  return 0;
+}
+
+
+//Solve linear system LL^Tx=b
+int alex_solve_LLTx(data_storage* mydata_storage,  double* b)
+{
+  return 0;
+}
+
+//Computing partial inverse, means solving linear system foer specific RHS
+int alex_inv(data_storage* mydata_storage,  pmatrix Q)
+{
+  return 0;
+}
+
+//Compute log-det
+int alex_log_det(data_storage* mydata_storage)
+{
+  return 0;
+}
+
+
+
+
+

80b8ac468fbae8e56086e1a1e3072e8387341cc6
Alexander Litvinenko <litvinen@tu-bs.de>
2018-01-16 20:17:32 +0300

    added alex_main() procedure

--- interface.c
+++ interface.c
@@ -13,13 +13,21 @@
 
 /*The structure which contains all required data*/
 struct data_storage{
-  int rows;
-  int cols;
+  /* Internal solver memory pointer pt,                  */
+  /* 32-bit: int pt[64]; 64-bit: long int pt[64]         */
+  /* or void *pt[64] should be OK on both architectures  */ 
+  void    *pt[64]; 
+
+  /* Pardiso control parameters. */
+  int      iparm[64];
+  double   dparm[64];
+  int      maxfct, mnum, phase, error, msglvl, solver;
+
+  /* Number of processors. */
+  int      num_procs;
   int nnz;
+  int n;
   int* perm;
-  int* ia;
-  int* ja;
-  double* vals;
   pspmatrix L;
 };
 typedef struct data_storage sdata_storage;
@@ -89,6 +97,16 @@ typedef smatrixQ* pmatrixQ;
 /* Allocation and initialization of PARDISO variables*/
 void alex_initialization(int flag)
 {
+   int      mtype = -2;        /* Real symmetric matrix */
+      //       mtype = 1;   /* NOT real symmetric matrix*/      
+   void    *pt[64];     /* Internal solver memory pointer pt,*/
+
+      /* Pardiso control parameters. */
+   int      iparm[64];
+   double   dparm[64];
+
+   pspmatrix Q = (pspmatrix) malloc(sizeof(sspmatrix));
+   pspmatrix L = (pspmatrix) malloc(sizeof(sspmatrix));
 
     /* Numbers of processors, value of OMP_NUM_THREADS */
     var = getenv("OMP_NUM_THREADS");
@@ -99,24 +117,19 @@ void alex_initialization(int flag)
         exit(1);
     }
     iparm[2]  = num_procs;
+    maxfct = 1;		/* Maximum number of numerical factorizations.  */
+    mnum   = 1;         /* Which factorization to use. */
+    msglvl = 1;         /* Print statistical information  */
+    error  = 0;         /* Initialize error flag */
 
 
-   pspmatrix Q = (pspmatrix) malloc(sizeof(sspmatrix));
-   pspmatrix L = (pspmatrix) malloc(sizeof(sspmatrix));
-//0="reordering"
-//1="symbolic factorization"
-//2="numerical factorization"
-//3="Cholesky factorization"
-//4="Partial inversion"
+//flag==0=="reordering"
+//flag==1="symbolic factorization"
+//flag==2="numerical factorization"
+//flag==3="Cholesky factorization"
+//flag==4="Partial inversion"
    if(flag==0) //0="reordering"
    {
-      int      mtype = -2;        /* Real symmetric matrix */
-      //       mtype = 1;   /* NOT real symmetric matrix*/      
-      void    *pt[64];     /* Internal solver memory pointer pt,*/
-
-      /* Pardiso control parameters. */
-      int      iparm[64];
-      double   dparm[64];
       int      maxfct, mnum, phase, error, msglvl, solver;
       int      num_procs;/* Number of processors. */
       char    *var;        /* Auxiliary variables. */
@@ -209,13 +222,11 @@ void read_sparse_matrix( pspmatrix Q)
 
 
 // reordering, computed just once
-int alex_reordering(pdata_storage mydata_storage, pgraph_t mgraph, int* mypermutation)
+int alex_reordering(pdata_storage mydata, pgraph_t mgraph, int* mypermutation)
 {
     clock_t start, end;
     double cpu_time_used;
 
-    FILE *f5;
-    int anzv=0, j=0, row_index=0, col_index=0;
     alex_initialization(1); // initialization for reordering
     pardisoinit (pt,  &mtype, &solver, iparm, dparm, &error); 
     if (error != 0) 
@@ -230,17 +241,17 @@ int alex_reordering(pdata_storage mydata_storage, pgraph_t mgraph, int* mypermut
     }
     else
         printf("[PARDISO]: License check was successful ... \n");
-    
     maxfct = 1;		/* Maximum number of numerical factorizations.  */
     mnum   = 1;         /* Which factorization to use. */
     msglvl = 1;         /* Print statistical information  */
     error  = 0;         /* Initialize error flag */
+    pdata_storage mydata_storage = NULL;
 /* -------------------------------------------------------------------- */
 /* ..  Reordering and Symbolic Factorization.  This step also allocates */
 /*     all memory that is necessary for the factorization.              */
 /* -------------------------------------------------------------------- */
     phase = 11; //analysis
-    read_sparse_matrix();
+    read_sparse_matrix(mydata_storage);
 
     pardiso (pt, &maxfct, &mnum, &mtype, &phase,
 	     &n, a, ia, ja, &idum, &nrhs,
@@ -260,7 +271,7 @@ int alex_reordering(pdata_storage mydata_storage, pgraph_t mgraph, int* mypermut
 
 
 //compute the symbolic factorization, computed just once
-int alex_symbolic_factorization(pdata_storage mydata_storage)
+int alex_symbolic_factorization(pdata_storage mystorage)
 {
     int      mtype = -2;        /* Real symmetric matrix */
     //    int      mtype = 1;   /* NOT real symmetric matrix*/      
@@ -310,13 +321,16 @@ int alex_symbolic_factorization(pdata_storage mydata_storage)
     mnum   = 1;         /* Which factorization to use. */
     msglvl = 1;         /* Print statistical information  */
     error  = 0;         /* Initialize error flag */
-/* -------------------------------------------------------------------- */
-/* ..  Reordering and Symbolic Factorization.  This step also allocates */
-/*     all memory that is necessary for the factorization.              */
-/* -------------------------------------------------------------------- */
+   /* -------------------------------------------------------------------- */
+   /* ..  Reordering and Symbolic Factorization.  This step also allocates */
+   /*     all memory that is necessary for the factorization.              */
+   /* -------------------------------------------------------------------- */
     phase = 11; //analysis
     read_sparse_matrix();
-
+    a=mystorage->a;
+    ia = mystorage->ia;
+    ja = mystorage->ja;
+    n = mystorage->n;
     pardiso (pt, &maxfct, &mnum, &mtype, &phase,
 	     &n, a, ia, ja, &idum, &nrhs,
              iparm, &msglvl, &ddum, &ddum, &error, dparm);
@@ -332,7 +346,7 @@ int alex_symbolic_factorization(pdata_storage mydata_storage)
 }
 
 //Numerical Cholesky factorization
-int alex_chol(pdata_storage mydata_storage, pmatrix Q)
+int alex_chol(pdata_storage mydata, pmatrix Q)
 {
    int phase = 22;
    iparm[32] = 1; /* compute determinant */
@@ -356,7 +370,7 @@ int alex_chol(pdata_storage mydata_storage, pmatrix Q)
 
 
 //Solve linear system Lx=b
-int alex_solve_Lx(pdata_storage mydata_storage,  double* b)
+int alex_solve_Lx(pdata_storage mydata,  double* b)
 {
 
 /* ..  Back substitution and iterative refinement.                      */
@@ -390,20 +404,23 @@ int alex_solve_Lx(pdata_storage mydata_storage,  double* b)
 
 
 //Solve linear system L^Tx=b
-int alex_solve_LTx(pdata_storage mydata_storage,  double* b)
+int alex_solve_LTx(pdata_storage mydata,  double* b)
 {
   return 0;
 }
 
 
 //Solve linear system LL^Tx=b
-int alex_solve_LLTx(pdata_storage mydata_storage,  double* b)
+int alex_solve_LLTx(pdata_storage mydata,  double* b)
 {
+  alex_solve_Lx(mydata,  b);
+  alex_solve_LTx(mydata, b);
+    
   return 0;
 }
 
 //Computing partial inverse, means solving linear system foer specific RHS
-int alex_inv(pdata_storage mydata_storage,  pmatrix Q)
+int alex_inv(pdata_storage mydata,  pmatrix Q)
 {
   /*On entry: IPARM(36) will control the selected inversion process based on the internal L and
   U factors. If IPARM(36) = 0 and PHASE = -22, PARDISO will overwrite these factors with
@@ -414,7 +431,7 @@ int alex_inv(pdata_storage mydata_storage,  pmatrix Q)
 }
 
 //Compute log-det
-double alex_log_det(data_storage* mydata_storage)
+double alex_log_det(data_storage* mydata)
 {
    double logdet=0.0;
    IPARM(33)=1;  //compute logdet
@@ -436,13 +453,86 @@ double alex_log_det(data_storage* mydata_storage)
 /* -------------------------------------------------------------------- */    
 /* ..  Termination and release of memory.                               */
 /* -------------------------------------------------------------------- */    
-int alex_finalize(pdata_storage mydata_storage)
+int alex_finalize(pdata_storage mydata)
 {
     int phase = -1;                 /* Release internal memory. */
     
     pardiso (pt, &maxfct, &mnum, &mtype, &phase,
              &n, &ddum, ia, ja, &idum, &nrhs,
              iparm, &msglvl, &ddum, &ddum, &error,  dparm);
+
+}
+
+
+/* To test the code run this procedure */
+int alex_main()
+{
+   clock_t start, end;
+   double cpu_time_used;
+   pdata_storage mydata;
+
+   int nnz;
+   int n;
+   int* perm;
+   pspmatrix L;
+   int mtype = -2;        /* Real symmetric matrix */
+
+    
+   double* b; /* RHS and solution vectors. */
+   double* x;
+   int nrhs = 1;          /* Number of right hand sides. */
+
+    /* Internal solver memory pointer pt,                  */
+    /* 32-bit: int pt[64]; 64-bit: long int pt[64]         */
+    /* or void *pt[64] should be OK on both architectures  */ 
+    void    *pt[64]; 
+    /* Pardiso control parameters. */
+    int      iparm[64];
+    double   dparm[64];
+    int      maxfct, mnum, phase, error, msglvl, solver;
+    int      num_procs;/* Number of processors. */
+    /* Auxiliary variables. */
+    char    *var;
+    int      i, k;
+    double   ddum;              /* Double dummy */
+    int      idum;              /* Integer dummy. */
+    pspmatrix Q, Qinv;
+    pgraph_t mgraph = NULL;
+    int* mypermutation = NULL;
+    /* RHS and solution vectors. */
+    double* b;
+    double* x;
+    int*  mypermutation;
+
+    mydata->mtype = -2;
+    mydata->rhs = 1;
+
+    mypermutation = (int*)malloc(n*sizeof(int)) ;
+    for( j = 0; j < n; j++)
+      mypermutation[j]=j;
+
+
+
+
+
+
+
+    read_sparse_matrix(Q, mydata);
+    alex_initialization(0, mydata); //flag==0=="reordering"
+    alex_reordering(mydata, mgraph, mypermutation);
+   
+    alex_initialization(1, mydata); //flag==1="symbolic factorization"
+    alex_initialization(2, mydata); //flag==2="numerical factorization"
+    alex_symbolic_factorization(mydata);
+
+    alex_initialization(3, mydata); //flag==3="Cholesky factorization"
+    alex_chol(mydata, Q);
+
+    alex_initialization(5, mydata); //flag==5="Solution"
+    alex_solve_LLTx(mydata, b, x);
+
+    alex_initialization(4, mydata); //flag==4="Partial inversion"
+    alex_inv(mydata, Q, Qinv);
 
 }
 
